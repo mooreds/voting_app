041f186d8f74453bfedd91a65cfe97cb112ce963
Dan Moore <github@mooreds.com>
2020-04-01 02:17:32 +0000

    added root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :voters
   resources :candidates
   resources :votes
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'votes#index'
 end
